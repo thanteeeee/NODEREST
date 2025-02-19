6f7bab8fe8808291e21f882a8b6eb60a67d767b3
thanteeeee <dandt270948@gmail.com>
2025-02-19 15:16:47 +0700

    upd_mongo

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 // Connect to the MongoDB database
-mongoose.connect('mongodb://admin:LQZkkg73929@node71378-node267tue.proen.app.ruk-com.cloud:11722', {
+mongoose.connect('mongodb://admin:HLPfyq50636@node71568-node30037js.proen.app.ruk-com.cloud:11770', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
@@ -66,6 +66,6 @@ app.delete('/books/:id', async (req, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log('API server is listening on port 3000');
+app.listen(5000, () => {
+  console.log('API server is listening on port 5000');
 });
